f642c92e326100662301fa96a66b6cc3d091aa3c
Peter Zhang <me@mby.pw>
2021-12-02 13:10:56 +0800

    new temporal consistency loss

--- SOURCE/losses.py
+++ SOURCE/losses.py
@@ -30,5 +30,11 @@ def g_loss_fn_3d(img_ab_real, img_ab_fake, class_vector_real, class_vector_pred,
     class_vector_real_ = merge_first_two_dims(class_vector_real)
     class_vector_pred_ = merge_first_two_dims(class_vector_pred)
     loss = g_loss_fn(img_ab_real_, img_ab_fake_, class_vector_real_, class_vector_pred_, gen_img_logits)
-    # TODO: new consistency loss
+
+    # consistency loss
+    img_ab_fake_1 = img_ab_fake[:, :-1]
+    img_ab_fake_2 = img_ab_fake[:, 1:]
+    img_ab_fake_1_ = merge_first_two_dims(img_ab_fake_1)
+    img_ab_fake_2_ = merge_first_two_dims(img_ab_fake_2)
+    loss += 0.2 * mse(img_ab_fake_1_, img_ab_fake_2_)
     return loss
